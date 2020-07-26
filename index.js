63974c4b8b9faacebb94a62fe923661a807b6c48
Vlad Omelyanenko <shield.deffence@gmail.com>
2020-07-26 14:42:06 +0300

    handlebars connection

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.sendFile(
-        path.join(__dirname, 'views', 'index.html')
-    )
+// registration and setup handlebars
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
 })
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 app.get('/about', (req, res) => {
-    res.sendFile(
-        path.join(__dirname, 'views', 'about.html')
-    )
+    res.render('about')
 })
 
 
