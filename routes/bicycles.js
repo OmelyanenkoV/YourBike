7b23aa431070bc7e9c60621edc83b8589abbe27f
Vlad Omelyanenko <47278133+worldofakk@users.noreply.github.com>
2020-07-27 13:45:14 +0300

    add list of bicycles

--- routes/bicycles.js
+++ routes/bicycles.js
@@ -1,10 +1,13 @@
 const {Router} = require('express')
+const Bicycle = require('../models/bicycle')
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const bicycles = await Bicycle.getAll()
     res.render('bicycles', {
         title: 'Велосипеды',
-        isBicycles: true
+        isBicycles: true,
+        bicycles
     })
 })
 
