16e607998b6088fa4490dc55db0a537914d8db5f
Vlad Omelyanenko <shield.deffence@gmail.com>
2020-07-29 22:00:54 +0300

    create model on mongodb

--- models/bicycle.js
+++ models/bicycle.js
@@ -1,83 +1,17 @@
-const uuid = require("uuid").v4;
-const fs = require("fs");
-const path = require("path");
-
-class Bicycle {
-  constructor(title, price, img) {
-    this.title = title;
-    this.price = price;
-    this.img = img;
-    this.id = uuid();
+const {Schema, model} = require('mongoose')
+const bicycle = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  img: {
+    type: String,
+    required: true
   }
+})
 
-  toJSON() {
-    return {
-      title: this.title,
-      price: this.price,
-      img: this.img,
-      id: this.id,
-    };
-  }
-
-  static async update(bicycle) {
-    const bicycles = await Bicycle.getAll();
-
-    const idx = bicycles.findIndex((c) => c.id === bicycle.id);
-    bicycles[idx] = bicycle;
-
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, "..", "data", "bicycles.json"),
-        JSON.stringify(bicycles),
-        (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        }
-      );
-    });
-  }
-
-  async save() {
-    const bicycles = await Bicycle.getAll();
-    bicycles.push(this.toJSON());
-
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, "..", "data", "bicycles.json"),
-        JSON.stringify(bicycles),
-        (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        }
-      );
-    });
-  }
-
-  static getAll() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(
-        path.join(__dirname, "..", "data", "bicycles.json"),
-        "utf-8",
-        (err, content) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(JSON.parse(content));
-          }
-        }
-      );
-    });
-  }
-  static async getById(id) {
-    const bicycles = await Bicycle.getAll();
-    return bicycles.find((c) => c.id === id);
-  }
-}
-
-module.exports = Bicycle;
+module.exports = model('Bicycle', bicycle)
--- routes/add.js
+++ routes/add.js
@@ -1,19 +1,26 @@
-const {Router} = require('express')
-const Bicycle = require('../models/bicycle')
-const router = Router()
+const { Router } = require("express");
+const Bicycle = require("../models/bicycle");
+const router = Router();
 
-router.get('/', (req, res) => {
-    res.render('add', {
-        title: 'Добавить велосипед',
-        isAdd: true
-    })
-})
+router.get("/", (req, res) => {
+  res.render("add", {
+    title: "Добавить велосипед",
+    isAdd: true,
+  });
+});
 
-router.post('/', async (req, res) => {
-    const bicycle = new Bicycle(req.body.title, req.body.price, req.body.img)
+router.post("/", async (req, res) => {
+  const bicycle = new Bicycle({
+    title: req.body.title,
+    price: req.body.price,
+    img: req.body.img,
+  });
+  try {
+    await bicycle.save();
+    res.redirect("/");
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-    await bicycle.save()
-    res.redirect('/')
-})
-
-module.exports = router
+module.exports = router;
